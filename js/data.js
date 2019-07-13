f44113a7fb13f95099e70952b751c55db4c5585f
Eric-chen <eric_pw@qq.com>
2019-07-13 23:29:57 +0800

    项目初始化

---
+++ js/data.js
@@ -0,0 +1,378 @@
+var phoneData = [
+
+    {
+        // 商品的id，用于区分每个商品的
+        "pID": 1,
+        // 商品的图片的路径
+        "imgSrc": "./images/0a15aa6747a614b0.jpg",
+        // 商品的名字
+        "name": "小米 红米Redmi Note7 幻彩渐变AI双摄 4GB+64GB 梦幻蓝 全网通4G 双卡双待 水滴全面屏拍照游戏智能手机",
+        // 商品的价格
+        "price": 3366,
+        // 售出的百分比
+        "percent": 69,
+        // 商品剩余多少
+        "left": 1481
+    },
+
+    {
+        "pID": 2,
+
+        "imgSrc": "./images/4f4ba51aed682207.jpg",
+
+
+        "name": "Apple iPhone XR (A2108) 128GB 黑色 移动联通电信4G手机 双卡双待",
+
+
+        "price": 1536,
+
+
+        "percent": 84,
+
+
+        "left": 459
+
+    },
+
+    {
+        "pID": 3,
+
+        "imgSrc": "./images/5a38e39b2975e837.jpg",
+
+
+        "name": "荣耀10青春版 幻彩渐变 2400万AI自拍 全网通版4GB+64GB 渐变蓝 移动联通电信4G全面屏手机 双卡双待",
+
+
+        "price": 1389,
+
+
+        "percent": 35,
+
+
+        "left": 759
+
+    },
+
+    {
+        "pID": 4,
+
+        "imgSrc": "./images/5ac1eae4Nce7c8b00.jpg",
+
+
+        "name": "小米8SE 全面屏智能游戏拍照手机 6GB+64GB 灰色 骁龙710处理器 全网通4G 双卡双待",
+
+
+        "price": 2730,
+
+
+        "percent": 76,
+
+
+        "left": 1788
+
+    },
+
+    {
+        "pID" : 5,
+
+        "imgSrc": "./images/5af52742Nfa205639.jpg",
+
+
+        "name": "华为 HUAWEI P20 Pro 全面屏徕卡三摄游戏手机 6GB+128GB 极光色 全网通移动联通电信4G手机 双卡双待",
+
+
+        "price": 3192,
+
+
+        "percent": 38,
+
+
+        "left": 753
+
+    },
+
+    {
+        "pID" : 6,
+
+        "imgSrc": "./images/5b0fbaabN3229c7a3.jpg",
+
+
+        "name": "荣耀V20 胡歌同款 麒麟980芯片 魅眼全视屏 4800万深感相机 6GB+128GB 幻夜黑 移动联通电信4G全面屏手机",
+
+
+        "price": 4158,
+
+
+        "percent": 22,
+
+
+        "left": 1060
+
+    },
+
+    {
+        "pID" : 7,
+
+        "imgSrc": "./images/5bf655e7Nb1528b53.jpg",
+
+
+        "name": "荣耀8X 千元屏霸 91%屏占比 2000万AI双摄 4GB+64GB 幻夜黑 移动联通电信4G全面屏手机 双卡双待",
+
+
+        "price": 4023,
+
+
+        "percent": 80,
+
+
+        "left": 1278
+
+    },
+
+    {
+        "pID" : 8,
+
+        "imgSrc": "./images/5bf67210N4b2507f1.jpg",
+
+
+        "name": "华为 HUAWEI nova 4e 3200万立体美颜AI超广角三摄珍珠屏6GB+128GB幻夜黑全网通版双4G手机",
+
+
+        "price": 3026,
+
+
+        "percent": 55,
+
+
+        "left": 1374
+
+    },
+
+    {
+        "pID" : 9,
+
+        "imgSrc": "./images/8ecf0cb424de1349.jpg",
+
+
+        "name": "小米9 4800万超广角三摄 8GB+128GB全息幻彩蓝 骁龙855 全网通4G 双卡双待 水滴全面屏拍照游戏智能手机",
+
+
+        "price": 4897,
+
+
+        "percent": 17,
+
+
+        "left": 201
+
+    },
+
+    {
+        "pID" : 10,
+
+        "imgSrc": "./images/59df2e7fN86c99a27.jpg",
+
+
+        "name": "小米 红米Redmi 7 AI双摄 3GB+32GB 亮黑色 全网通4G 双卡双待 水滴全面屏拍照游戏智能手机",
+
+
+        "price": 523,
+
+
+        "percent": 54,
+
+
+        "left": 237
+
+    },
+
+    {
+        "pID" : 11,
+
+        "imgSrc": "./images/659cfc78ee5678e2.jpg",
+
+
+        "name": "Apple iPhone XS Max (A2104) 256GB 深空灰色 移动联通电信4G手机 双卡双待",
+
+
+        "price": 1379,
+
+
+        "percent": 80,
+
+
+        "left": 1032
+
+    },
+
+    {
+        "pID" : 12,
+
+        "imgSrc": "./images/819dcf182d743897.jpg",
+
+
+        "name": "华为 HUAWEI P30 超感光徕卡三摄麒麟980AI智能芯片全面屏屏内指纹版手机8GB+128GB天空之境全网通双4G手机",
+
+
+        "price": 2954,
+
+
+        "percent": 42,
+
+
+        "left": 1068
+
+    },
+
+    {
+        "pID" : 13,
+
+        "imgSrc": "./images/865565d919219154.jpg",
+
+
+        "name": "荣耀畅玩8C两天一充 莱茵护眼 刘海屏 全网通版4GB+32GB 幻夜黑 移动联通电信4G全面屏手机 双卡双待",
+
+
+        "price": 2353,
+
+
+        "percent": 15,
+
+
+        "left": 1483
+
+    },
+
+    {
+        "pID" : 14,
+
+        "imgSrc": "./images/a2c208410ae84d1f.jpg",
+
+
+        "name": "华为 HUAWEI P30 Pro 超感光徕卡四摄10倍混合变焦麒麟980芯片屏内指纹 8GB+128GB亮黑色全网通版双4G手机",
+
+
+        "price": 3335,
+
+
+        "percent": 89,
+
+
+        "left": 251
+
+    },
+
+    {
+        "pID" : 15,
+
+        "imgSrc": "./images/ac1181ba1660dc64.png",
+
+
+        "name": "小米9 SE 4800万超广角三摄 骁龙712 水滴全面屏 游戏智能拍照手机 6GB+128GB 深空灰 全网通4G 双卡双待",
+
+
+        "price": 2514,
+
+
+        "percent": 44,
+
+
+        "left": 1858
+
+    },
+
+    {
+        "pID" : 16,
+
+        "imgSrc": "./images/c16e93d0bf77a31f.jpg",
+
+
+        "name": "Apple iPhone X (A1865) 64GB 深空灰色 移动联通电信4G手机",
+
+
+        "price": 3287,
+
+
+        "percent": 8,
+
+
+        "left": 481
+
+    },
+
+    {
+        "pID" : 17,
+
+        "imgSrc": "./images/ce6c1dee969fb626.jpg",
+
+
+        "name": "荣耀10 GT游戏加速 AIS手持夜景 6GB+64GB 幻夜黑 全网通 移动联通电信4G 双卡双待 游戏手机",
+
+
+        "price": 2305,
+
+
+        "percent": 57,
+
+
+        "left": 1191
+
+    },
+
+    {
+        "pID" : 18,
+
+        "imgSrc": "./images/dbe080c29fb0aeff.jpg",
+
+
+        "name": "华为 HUAWEI 畅享9  4GB+64GB 幻夜黑 高清珍珠屏 AI长续航 全网通高配版 移动联通电信4G手机",
+
+
+        "price": 681,
+
+
+        "percent": 82,
+
+
+        "left": 547
+
+    },
+
+    {
+        "pID" : 19,
+
+        "imgSrc": "./images/e60896d2743a08b8.jpg",
+
+
+        "name": "小米 红米6A AI美颜 3GB+32GB 流沙金 全网通4G手机 双卡双待",
+
+
+        "price": 2319,
+
+
+        "percent": 21,
+
+
+        "left": 1020
+
+    },
+
+    {
+        "pID" : 20,
+
+        "imgSrc": "./images/fb7b968a62eb10ef.jpg",
+
+
+        "name": "华为 HUAWEI Mate 20 Pro (UD)屏内指纹版麒麟980芯片全面屏超大广角徕卡三摄8GB+128GB亮黑色全网通双4G手机",
+
+
+        "price": 1119,
+
+
+        "percent": 26,
+
+
+        "left": 1434
+
+    }
+]
