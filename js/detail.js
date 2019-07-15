b17aac32053bfdaffefab3934b1a96e167b8651f
Eric-chen <eric_pw@qq.com>
2019-07-15 21:05:28 +0800

    点击加入购物车并将数据保存在本地

--- js/detail.js
+++ js/detail.js
@@ -6,4 +6,28 @@ $(() => {
     $('.sku-name').text(obj.name);
     $('.preview-img img').attr('src', obj.imgSrc);
     $('.summary-price em').text(obj.price);
+
+
+    // 点击加入购物车
+    $('.addshopcar').on('click', function () {
+        let number = parseInt($('.choose-amount .choose-number').val());
+        let arr = loadData('shopCartData');
+        let isExit = arr.find(e => {
+            return e.pID === id;
+        })
+        if (isExit !== undefined) {
+            isExit.number += number;
+        } else {
+            let good = {
+                pID: obj.pID,
+                name: obj.name,
+                imgSrc: obj.imgSrc,
+                price: obj.price,
+                number: number
+            }
+            arr.push(good);
+        }
+        saveData('shopCartData', arr);
+        location.href = "cart.html";
+    })
 })
